793dd44299e2f930581e4e33e0e3097efcd8c2b6
jpacaonces <janssen.m.pacaonces@gmail.com>
2026-09-12 01:07:53 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IT_ELECTIVE_2_BSIT31E1_PREFINAL_EXAM_Pacaonces_Janssen.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
